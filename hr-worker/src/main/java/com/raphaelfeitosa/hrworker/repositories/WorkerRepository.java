564f32aa8f8cefc16e801b36532fe14b110f85d6
Raphael Feitosa <raphaelcs2@gmail.com>
2021-05-19 17:38:26 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/raphaelfeitosa/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,11 @@
+package com.raphaelfeitosa.hrworker.repositories;
+
+import com.raphaelfeitosa.hrworker.entities.Worker;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+}
